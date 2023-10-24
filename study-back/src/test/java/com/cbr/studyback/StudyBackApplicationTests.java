bf7a0f6e9e2ed3cd3b6ce8537f934f76647adf0d
wjBoram <rnumch@gmail.com>
2023-10-24 16:24:54 +0900

    1. 최초커밋 진행

---
+++ study-back/src/test/java/com/cbr/studyback/StudyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbr.studyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
